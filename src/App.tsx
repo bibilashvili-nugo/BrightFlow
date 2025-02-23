f52eef75e3e3800bd387db2c0484959b426aaa63
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-02-23 01:46:21 -0800

    test

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-[300px] h-[200px] bg-red-200">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
