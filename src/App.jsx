efc4e0c3fc51a97ec314cc01d7d784e32042679c
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-02-23 02:04:13 -0800

    test

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-amber-500">
+      <h1 className="font-light text-8xl">nugooo</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="w-[300px] h-[200px] bg-red-200">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
-}
-
-export default App;
