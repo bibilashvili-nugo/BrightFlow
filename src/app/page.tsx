da0007c4ecaaabb00afbdc91ad2fc40f300f1f29
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-02-25 15:01:01 -0800

    input colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -138,6 +138,7 @@ export default function Home() {
                 name={formData.name}
                 email={formData.email}
                 onChange={handleChange}
+                disabled={loading}
               />
             </div>
             <div className="smaller:pb-0 pb-3 xl:pb-0 w-full">
--- src/components/FormInputs.tsx
+++ src/components/FormInputs.tsx
@@ -4,9 +4,15 @@ type FormInputsProps = {
   name: string;
   email: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled: boolean;
 };
 
-const FormInputs: React.FC<FormInputsProps> = ({ name, email, onChange }) => {
+const FormInputs: React.FC<FormInputsProps> = ({
+  name,
+  email,
+  onChange,
+  disabled,
+}) => {
   return (
     <div className="flex flex-col xl:flex-row gap-4 w-full">
       <input
@@ -14,29 +20,37 @@ const FormInputs: React.FC<FormInputsProps> = ({ name, email, onChange }) => {
         value={name}
         onChange={onChange}
         placeholder="სახელი და გვარი"
-        className={`rounded-[8px] border border-white border-opacity-10 bg-[#2B2B2B] w-full h-[48px] md:w-[530px]
+        className={`rounded-[8px] border border-white border-opacity-10 bg-[#1D1D1D] w-full h-[48px] md:w-[530px]
         placeholder:text-[#A0A2A6] placeholder:font-medium placeholder:text-[12px] text-[#A0A2A6] text-xs leading-[18px] 
         p-y-[15px] pl-[12px] 
         xl:w-[269px] 
-        hover:outline hover:outline-[1px] hover:outline-[#555555 ] 
         focus:ring-0 focus:outline focus:outline-[1px] focus:border-transparent
         focus:outline-[#4397F7] focus:outline-offset-[0px] transition-colors duration-300
-        smaller:mb-0 xl:mb-0
+        smaller:mb-0 xl:mb-0 ${
+          disabled
+            ? "opacity-50 cursor-not-allowed"
+            : "hover:outline hover:outline-[1px] hover:outline-[#505050]"
+        }
         `}
+        disabled={disabled}
       />
       <input
         name="email"
         value={email}
         onChange={onChange}
         placeholder="თქვენი ელ.ფოსტა"
-        className={`rounded-[8px] border border-white border-opacity-10 bg-[#2B2B2B] w-full h-[48px] md:w-[530px]
+        className={`rounded-[8px] border border-white border-opacity-10 bg-[#1D1D1D] w-full h-[48px] md:w-[530px]
         placeholder:text-[#A0A2A6] placeholder:font-medium placeholder:text-[12px] text-[#A0A2A6] p-y-[15px] pl-[12px] 
         xl:w-[269px] text-xs leading-[18px]
-        hover:outline hover:outline-[1px] hover:outline-[#555555 ] 
         focus:ring-0 focus:outline focus:outline-[1px] focus:border-transparent
         focus:outline-[#4397F7] focus:outline-offset-[0px] transition-colors duration-300
-        smaller:mb-0 mb-6 xl:mb-0
+        smaller:mb-0 mb-6 xl:mb-0 ${
+          disabled
+            ? "opacity-50 cursor-not-allowed"
+            : "hover:outline hover:outline-[1px] hover:outline-[#505050]"
+        }
         `}
+        disabled={disabled}
       />
     </div>
   );
