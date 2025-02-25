11971f65064d1082960abfd2a6349d2234f9ef1d
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-02-25 14:49:42 -0800

    text size in inputs

--- src/components/FormInputs.tsx
+++ src/components/FormInputs.tsx
@@ -15,7 +15,8 @@ const FormInputs: React.FC<FormInputsProps> = ({ name, email, onChange }) => {
         onChange={onChange}
         placeholder="სახელი და გვარი"
         className={`rounded-[8px] border border-white border-opacity-10 bg-[#2B2B2B] w-full h-[48px] md:w-[530px]
-        placeholder:text-[#A0A2A6] placeholder:font-medium placeholder:text-[12px] text-[#A0A2A6] p-y-[15px] pl-[12px] 
+        placeholder:text-[#A0A2A6] placeholder:font-medium placeholder:text-[12px] text-[#A0A2A6] text-xs leading-[18px] 
+        p-y-[15px] pl-[12px] 
         xl:w-[269px] 
         hover:outline hover:outline-[1px] hover:outline-[#555555 ] 
         focus:ring-0 focus:outline focus:outline-[1px] focus:border-transparent
@@ -30,7 +31,7 @@ const FormInputs: React.FC<FormInputsProps> = ({ name, email, onChange }) => {
         placeholder="თქვენი ელ.ფოსტა"
         className={`rounded-[8px] border border-white border-opacity-10 bg-[#2B2B2B] w-full h-[48px] md:w-[530px]
         placeholder:text-[#A0A2A6] placeholder:font-medium placeholder:text-[12px] text-[#A0A2A6] p-y-[15px] pl-[12px] 
-        xl:w-[269px] 
+        xl:w-[269px] text-xs leading-[18px]
         hover:outline hover:outline-[1px] hover:outline-[#555555 ] 
         focus:ring-0 focus:outline focus:outline-[1px] focus:border-transparent
         focus:outline-[#4397F7] focus:outline-offset-[0px] transition-colors duration-300
