458b4b00dbfbf9d6fd5d8da0c19d1fc2dd5dee2d
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-02-25 14:06:33 -0800

    button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,9 +17,9 @@ const Button: React.FC<ButtonProps> = ({
     <button
       type={type}
       onClick={onClick}
-      className={`bg-[#002FEF] h-12 rounded-[8px] font-semibold text-sm text-white
-         hover:bg-[#042BCE] focus:bg-[#002FEF] focus:ring-[3px] focus:ring-white 
-         focus:ring-opacity-10 active:bg-[#0426B5] ${className} smaller:w-full`}
+      className={`bg-[#0766FF] h-12 rounded-[8px] font-semibold text-sm text-white
+         hover:bg-[#0054DA] focus:bg-[#0766FF] focus:ring-[3px] focus:ring-white 
+         focus:ring-opacity-10 active:bg-[#0049BE] ${className} smaller:w-full`}
     >
       {text}
     </button>
