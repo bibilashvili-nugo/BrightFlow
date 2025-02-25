d4e299b23cfe0228519d210482dce1376e726a30
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-02-25 05:13:36 -0800

    fix inputs

--- src/components/messageBox.tsx
+++ src/components/messageBox.tsx
@@ -46,9 +46,9 @@ export default function MessageBox({ checkStatus, email }: Status) {
   } else {
     return (
       <div className="w-[448px] md:w-[530px] xl:w-[386px] border border-[#FD411B] border-opacity-20 rounded-xl pl-4 py-4 pr-[18px]">
-        <div className="flex justify-between place-items-start">
+        <div className="flex justify-between place-items-start gap-3">
           <Image src="/allerts.png" alt="circus check" width={24} height={24} />
-          <span className="text-sm text-[#A0A2A6] w-[284px]">
+          <span className="text-sm text-[#A0A2A6] w-[384px]">
             {`შენს მიერ შეყვენილი მონაცემები არასწორია!`}
           </span>
           <Image
